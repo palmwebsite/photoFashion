1a1c444364ae3e486c79f55536573e34235dbf55
palmwebsite <madis.palm@gmail.com>
2024-11-24 15:41:54 +0200

    madise esimene kommit

--- src/app/kontakt/conf.ts
+++ src/app/kontakt/conf.ts
@@ -2,6 +2,6 @@ import { IListItem } from "../components/layouts/List";
 import trans from "./texts.et.json";
 
 export const CONTACTS: IListItem[] = [
-  { label: trans.email, value: "example@email.com" },
+  { label: trans.email, value: "päris emaili aadress@email.com" },
   { label: trans.phone, value: "1234567890" },
 ];
