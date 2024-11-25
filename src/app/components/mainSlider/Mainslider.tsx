3918ed8231c13452206df958539f8da63c7f277c
Liisi29 <liisi.raidaru@gmail.com>
2024-11-25 15:22:31 -0500

    tried out portrait/landscape

--- src/app/components/mainSlider/Mainslider.tsx
+++ src/app/components/mainSlider/Mainslider.tsx
@@ -2,20 +2,27 @@
 
 import { useScreenOrientation } from "@/app/hooks/useScreenOrientation";
 import SwiperComponent from "../swiper/SwiperComponent";
-import { PHOTOS } from "./conf";
-import { useEffect } from "react";
+import { getPhotos } from "./conf";
+import { useEffect, useState } from "react";
+import { ISlide } from "../dto";
 
 export function MainSlider() {
+  const [images, setImages] = useState<ISlide[]>([]);
   const [orientation] = useScreenOrientation();
 
   useEffect(() => {
-    const isLandscape = orientation.includes("landscape");
     const isPortrait = orientation.includes("portrait");
-    console.log("Orientation: ", orientation);
+    let _images = [];
+    if (isPortrait) {
+      _images = getPhotos("vertical");
+    } else {
+      _images = getPhotos("horizontal");
+    }
+    setImages(_images);
   }, [orientation]);
   return (
     <SwiperComponent
-      images={PHOTOS}
+      images={images}
       wantsTextOnNavigation={true}
       wantsZoomEffect={true}
       wantsCover={true}
--- src/app/components/mainSlider/conf.ts
+++ src/app/components/mainSlider/conf.ts
@@ -1,70 +1,95 @@
-import { ISlide } from "../dto";
+import { IImageBase, ISlide, ISlideHref } from "../dto";
 import { goToGalleryWithFilter, URLS } from "../menu/urls";
 import trans from "./texts.et.json";
 
-export const PHOTOS: ISlide[] = [
+// Kui slaidi lisad, pead siia ka uue teksti lisama (kui talle linki tahad peale)
+const TEXTS: ISlideHref[] = [
+  {
+    h1: trans.firstH1,
+    h2: trans.firstH2,
+    href: goToGalleryWithFilter("loom"),
+  },
+  {
+    h1: trans.scndH1,
+    h2: trans.scndH2,
+    href: URLS.about,
+  },
+  {
+    h1: trans.thirdH1,
+    h2: trans.thirdH2,
+    href: URLS.about,
+  },
+  {
+    h1: trans.fourH1,
+    h2: trans.fourH2,
+    href: URLS.about,
+  },
+  {
+    h1: trans.fiveH1,
+    h2: trans.fiveH2,
+    href: URLS.about,
+  },
+  {
+    h1: trans.sixH1,
+    h2: trans.sixH2,
+    href: URLS.about,
+  },
+];
+const HORIZONTAL_PHOTOS: IImageBase[] = [
   {
     id: "mic",
     alt: "Mic",
-    isHorizontal: true,
-    title: {
-      h1: trans.firstH1,
-      h2: trans.firstH2,
-      href: goToGalleryWithFilter("loom"),
-    },
   },
   {
     id: "brown",
 
     alt: "Brown",
-    isHorizontal: true,
-    title: {
-      h1: trans.scndH1,
-      h2: trans.scndH2,
-      href: URLS.about,
-    },
   },
   {
     id: "coffee",
     alt: "Coffee",
-    isHorizontal: true,
-    title: {
-      h1: trans.thirdH1,
-      h2: trans.thirdH2,
-      href: URLS.about,
-    },
   },
   {
     id: "foggy",
 
     alt: "Foggy",
-    isHorizontal: true,
-    title: {
-      h1: trans.fourH1,
-      h2: trans.fourH2,
-      href: URLS.about,
-    },
   },
   {
     id: "wolf",
 
     alt: "Wolf",
-    isHorizontal: true,
-    title: {
-      h1: trans.fiveH1,
-      h2: trans.fiveH2,
-      href: URLS.about,
-    },
   },
   {
     id: "beach",
 
     alt: "beach",
-    isHorizontal: true,
-    title: {
-      h1: trans.sixH1,
-      h2: trans.sixH2,
-      href: URLS.about,
-    },
   },
 ];
+
+const VERTICAL_PHOTOS: IImageBase[] = [
+  {
+    id: "kasukad",
+    alt: "kasukad",
+  },
+  {
+    id: "kleit",
+
+    alt: "kleit",
+  },
+  {
+    id: "seelik",
+    alt: "seelik",
+  },
+];
+
+export function getPhotos(type: "vertical" | "horizontal"): ISlide[] {
+  const _images = type === "horizontal" ? HORIZONTAL_PHOTOS : VERTICAL_PHOTOS;
+  return _images.map((img, index: number) => {
+    const slide: ISlide = {
+      ...img,
+      title: TEXTS[index],
+      isHorizontal: type === "horizontal",
+    };
+    return slide;
+  });
+}
