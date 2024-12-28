0334f1294303fc1d5d919593f81f89e615642fd2
Liisi29 <liisi.raidaru@gmail.com>
2024-12-28 05:25:14 -0500

    contact

--- src/app/components/layouts/List.tsx
+++ src/app/components/layouts/List.tsx
@@ -3,18 +3,22 @@ interface IProps {
   arr: IListItem[];
 }
 export interface IListItem {
-  label: string;
-  value: string;
+  label?: string;
+  value: string | JSX.Element;
 }
 export function List(props: IProps) {
   return (
     <div>
-      {props.arr.map((item) => (
-        <div className={styles.listItem} key={item.value}>
+      {props.arr.map((item, index) => (
+        <div className={styles.listItem} key={index}>
           <div className={styles.listLabel}>{item.label}</div>
           <div className={styles.listValue}>{item.value}</div>
         </div>
       ))}
     </div>
   );
+}
+
+export function ListItem(props: IListItem) {
+  return <div className={styles.listValue}>{props.value}</div>;
 }
--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,25 +1,25 @@
-import { Description } from "../components/layouts/Description";
 import { Header } from "../components/layouts/Header";
 import { Page } from "../components/layouts/Page";
-import { IListItem, List } from "../components/layouts/List";
+import { ListItem } from "../components/layouts/List";
 
 const texts = {
   title: "Kontakt",
-  description: "",
-  email: "",
-  phone: "",
 };
-const CONTACTS: IListItem[] = [
-  { label: texts.email, value: "madis.palm@gmail.com" },
-  { label: texts.phone, value: "+3725293128" },
-];
+const email = "madis.palm@gmail.com";
+const phone = "+3725293128";
 
 export default function Kontakt() {
   return (
     <Page width="900px">
       <Header>{texts.title}</Header>
-      <Description>{texts.description}</Description>
-      <List arr={CONTACTS} />
+      <ListItem value={phone} />
+      <ListItem
+        value={
+          <a href={`mailto:${email}?subject=Fotograaf Madis Palm&body=Tere.`}>
+            {email}
+          </a>
+        }
+      />
     </Page>
   );
 }
